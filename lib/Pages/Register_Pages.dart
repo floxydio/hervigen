62de0ec1d68b9d2127417fcdf750a9f820fdd28b
Mercuriallz <ramandadiio@gmail.com>
2022-01-29 21:52:06 +0700

    sabtu

---
+++ lib/Pages/Register_Pages.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hervigen/Service/Font_Style.dart';
+
+class RegisterPages extends StatefulWidget {
+  const RegisterPages({ Key? key }) : super(key: key);
+
+  @override
+  _RegisterPagesState createState() => _RegisterPagesState();
+}
+
+class _RegisterPagesState extends State<RegisterPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        leading: IconButton(icon: Icon(Icons.arrow_left_sharp),
+        color: Colors.white,
+        iconSize: 40,
+        onPressed: () {},),
+        centerTitle: true,
+        title: Text("Viginesia", style: boardStyle,),
+      ),
+      body: SafeArea(child: 
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.topLeft, 
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Text("Daftar", style: registerTitleStyle,),
+          ),
+        ],
+      )),
+      
+      
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hervigen/Pages/Register_Pages.dart';
 
 import 'Pages/Login_Pages.dart';
 
 void main() => runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: AuthPagesLogin()));
+    const MaterialApp(debugShowCheckedModeBanner: false, home: RegisterPages()));
 
