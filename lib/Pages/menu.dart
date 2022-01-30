c284cfbe1d0ba136c24cf79778132355e4e3702c
MuchamadIqbaal08 <iqbaalmuchamad31@gmail.com>
2022-01-31 01:18:06 +0700

    update

--- lib/Pages/menu.dart
+++ lib/Pages/menu.dart
@@ -1,8 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hervigen/Pages/Profile.dart';
 import 'package:hervigen/service/service_api.dart';
 
 class Menu extends StatefulWidget {
-  const Menu({Key? key}) : super(key: key);
+  final dynamic idUser;
+  final String? email;
+  final String? profesi;
+  final String? password;
+  final String? nama;
+  const Menu(
+      {Key? key,
+      this.idUser,
+      this.email,
+      this.profesi,
+      this.password,
+      this.nama})
+      : super(key: key);
 
   @override
   State<Menu> createState() => _MenuState();
@@ -10,25 +24,44 @@ class Menu extends StatefulWidget {
 
 class _MenuState extends State<Menu> {
   TextEditingController motivasiController = TextEditingController();
+  List? dataMotivasi = [];
 
   @override
   void initState() {
+    ServiceApi().motivasiList().then((value) => {
+          dataMotivasi = [],
+          setState(() {
+            dataMotivasi = value;
+          })
+        });
     super.initState();
-    ServiceApi().motivasiList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 15.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Column(children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => MyProfile(
+                                  email: widget.email,
+                                  idUser: widget.idUser,
+                                  nama: widget.nama,
+                                  password: widget.password,
+                                  profesi: widget.profesi,
+                                )));
+                  },
+                  child: const Text("Go TO Profile")),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
                 SizedBox(
-                  width: MediaQuery.of(context).size.width / 1.4,
+                  width: MediaQuery.of(context).size.width / 1.3,
                   child: TextFormField(
                     controller: motivasiController,
                     decoration: InputDecoration(
@@ -41,15 +74,123 @@ class _MenuState extends State<Menu> {
                 ElevatedButton(
                     onPressed: () {
                       setState(() {
-                        ServiceApi()
-                            .sendMotivation(motivasiController.text)
-                            .then((value) => ServiceApi().motivasiList());
+                        if (kDebugMode) {
+                          print(widget.idUser);
+                        }
+                        if (kDebugMode) {
+                          print(motivasiController.text);
+                        }
+                        ServiceApi().sendMotivation(
+                            motivasiController.text, widget.idUser);
+                        setState(() {
+                          motivasiController.text == "";
+                          ServiceApi()
+                              .motivasiList()
+                              .then((value) => {dataMotivasi = value});
+                        });
                       });
                     },
-                    child: const Text("Post"))
-              ],
-            )
-          ]),
+                    child: const Text("Post")),
+              ]),
+              dataMotivasi!.isEmpty
+                  ? const CircularProgressIndicator()
+                  : Padding(
+                      padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+                      child: ListView.builder(
+                          physics: const ScrollPhysics(),
+                          itemCount: 10,
+                          shrinkWrap: true,
+                          itemBuilder: (context, i) {
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 15.0),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 0.5, color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(15)),
+                              child: ListTile(
+                                leading: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("${i + 1}"),
+                                  ],
+                                ),
+                                title: Text(dataMotivasi![i]["isi_motivasi"]),
+                                subtitle: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 15.0, bottom: 15.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      InkWell(
+                                        onTap: () {
+                                          AlertDialog alert = AlertDialog(
+                                            content: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                const Text(
+                                                    "Edit motivasi kamu"),
+                                                Container(
+                                                  margin: const EdgeInsets.only(
+                                                      top: 10.0, bottom: 10.0),
+                                                  child: TextFormField(
+                                                    initialValue:
+                                                        motivasiController.text,
+                                                    maxLines: 8,
+                                                    onChanged: (value) {
+                                                      setState(() {});
+                                                    },
+                                                    decoration: InputDecoration(
+                                                      border:
+                                                          OutlineInputBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          20)),
+                                                    ),
+                                                  ),
+                                                ),
+                                                ElevatedButton(
+                                                    onPressed: () async {
+                                                      ServiceApi()
+                                                          .editMotivation(
+                                                              motivasiController
+                                                                  .text,
+                                                              widget.idUser);
+                                                    },
+                                                    child: const Text("Ubah"))
+                                              ],
+                                            ),
+                                          );
+
+                                          // show the dialog
+                                          showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return alert;
+                                            },
+                                          );
+                                        },
+                                        child: const Icon(Icons.edit),
+                                      ),
+                                      const SizedBox(width: 15),
+                                      InkWell(
+                                        onTap: () {
+                                          setState(() {
+                                            ServiceApi().deleteMotivation(
+                                                widget.idUser);
+                                          });
+                                        },
+                                        child: const Icon(Icons.delete),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    )
+            ]),
+          ),
         ),
       ),
     );
